eab57388b5402e02b0a8db89e6cd2099f0ce01f9
Catherine  Hull <cathh127@hotmail.com>
2024-04-02 13:57:11 +0000

    Add buttons

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,18 +1,20 @@
-function getData(cb) {
-    var xhr = new XMLHttpRequest();
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
 
-    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-    xhr.send();
+function getData(type, cb) {
+    var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
     };
-}
 
-function printDataToConsole(data) {
-    console.log(data);
-}
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+    }
 
-getData(printDataToConsole);
+function writeToDocument(type) {
+    getData(type, function(data){
+        document.getElementById("data").innerHTML = data;
+    });
+}
